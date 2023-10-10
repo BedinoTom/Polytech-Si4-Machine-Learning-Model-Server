125663f7fadca6daaaf2d2637928a5aca7083617
Tom Bedino <tom.bedino@etu.univ-cotedazur.fr>
2023-10-10 17:26:00 +0200

    feat: add proccess endpoint

--- app/main.py
+++ app/main.py
@@ -8,13 +8,16 @@ app = FastAPI()
 class Model(BaseModel):
     name: str
     id: str
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/list")
-async def model_list() -> list[Model]:
+    
+class RequestImage(BaseModel):
+    model_id: str
+    image_raw: str
+    
+class ResponseImage(BaseModel):
+    model_id: str
+    number_class: str
+    
+def get_model_list():
     fp = open(os.path.join(os.getcwd(), "app", "models.json"))
     data = json.load(fp)
     models = data["models"]
@@ -22,3 +25,24 @@ async def model_list() -> list[Model]:
     for model in models:
         result.append(Model(name=model["name"], id=model["id"]))
     return result
+
+def get_model_by_id(id):
+    models = get_model_list()
+    for model in models:
+        if model.id == id:
+            return model
+    return None
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/proccess")
+async def proccess(req: RequestImage) -> ResponseImage:
+    model = get_model_by_id(req.model_id)
+    if model == None:
+        return ResponseImage(model_id="none", number_class="-1")
+
+@app.get("/list")
+async def model_list() -> list[Model]:
+    return get_model_list()
