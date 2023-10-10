002fc1c671a0104dbf08aa4d079b9c7947c43b29
Tom Bedino <tom.bedino@etu.univ-cotedazur.fr>
2023-10-10 13:38:12 +0200

    feat: add list model

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+import os
 
 app = FastAPI()
 
+class Model(BaseModel):
+    name: str
+    id: str
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/list")
+async def model_list() -> list[Model]:
+    fp = open(os.path.join(os.getcwd(), "app", "models.json"))
+    data = json.load(fp)
+    models = data["models"]
+    result = []
+    for model in models:
+        result.append(Model(name=model["name"], id=model["id"]))
+    return result
