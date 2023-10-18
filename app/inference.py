5266c105bbf7ea60e5bcbd0dc944329e3bba6174
Tom Bedino <tom.bedino@etu.univ-cotedazur.fr>
2023-10-18 17:11:50 +0200

    fix: correct tsne

--- app/inference.py
+++ app/inference.py
@@ -1,15 +1,15 @@
 import numpy as np
 import pickle
 import os
-from sklearn import tree
+from sklearn import datasets
 from sklearn import manifold
-from sklearn.cluster import KMeans
-from sklearn_extra.cluster import KMedoids
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
 from skimage import color
 from skimage.transform import resize
+
+def load_dataset():
+    X, _ = datasets.load_digits(return_X_y=True)
+    return X
 
 def load_model(model_path):
     file = open(model_path, 'rb')
@@ -17,13 +17,13 @@ def load_model(model_path):
     file.close()
     return model
 
-def proccess_standard_scaler(X):
+def proccess_standard_scaler(dataset, X):
     scaler = StandardScaler()
-    return scaler.fit_transform(X)
+    return scaler.fit_transform(np.concatenate((dataset,[X]), axis=0))[0]
 
-def proccess_t_sne(X, component):
+def proccess_t_sne(dataset, X, component):
     tsne = manifold.TSNE(n_components=component, init='pca', random_state=0)
-    return tsne.fit_transform(X)
+    return tsne.fit_transform(np.concatenate((dataset,[X]), axis=0))[0]
 
 def transform_image(np_image):
     if len(np_image.shape) == 3 and (np_image.shape[2] ==3):
@@ -36,13 +36,14 @@ def transform_image(np_image):
                        anti_aliasing=True)
 
 def inference(model_param, np_image):
+    dataset = load_dataset()
     model = load_model(model_path=os.path.join(os.getcwd(), "app", "models", model_param["file"]))
     prepare_image = transform_image(np_image).reshape((64,))
     
     if model_param["std"]:
-        prepare_image = proccess_standard_scaler(prepare_image)
+        prepare_image = proccess_standard_scaler(dataset, prepare_image)
     if model_param["tsne"]:
-        prepare_image = proccess_t_sne(prepare_image, int(model_param["tsne_component"]))
+        prepare_image = proccess_t_sne(dataset, prepare_image, int(model_param["tsne_component"]))
         
     try:
         result = model.predict([prepare_image])
